b359856303bc0eebfce5e4f9e496e2f2a781e868
PwnyTail <pwnytail@c3d2.de>
2019-11-16 15:44:06 +0100

    update teamspeak url

--- teamspeak.sh
+++ teamspeak.sh
@@ -4,8 +4,8 @@ TEAMSPEAK_HOME=$SERVER_HOME/teamspeak
 mkdir -p $TEAMSPEAK_HOME
 cd $TEAMSPEAK_HOME
 
-wget -c http://teamspeak.gameserver.gamed.de/ts3/releases/3.0.12.4/teamspeak3-server_linux_amd64-3.0.12.4.tar.bz2
-tar xfvj teamspeak3-server_linux_amd64-3.0.12.4.tar.bz2
+wget -c https://files.teamspeak-services.com/releases/server/3.10.0/teamspeak3-server_linux_amd64-3.10.0.tar.bz2
+tar xfvj teamspeak3-server_linux_amd64-3.10.0.tar.bz2
 
 echo -e "
 Start Server
